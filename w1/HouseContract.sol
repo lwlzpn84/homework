6131c22887940313821e2e74f6e939ef0c286253
luwl_free <luwl_free@163.com>
2022-02-23 20:11:48 +0800

    使用 Remix 创建任意一个合约

---
+++ w1/HouseContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract HouseContract {
+    uint public num;
+    constructor(uint n) {
+        num = n;
+    }
+
+    function add(uint n) public {
+        num = num + n;
+    }
+
+    function get() public view returns(uint) {
+        return num;
+    }
+}
